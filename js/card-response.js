e8b8d642727f5861a52e4edb1dd9576f8f0172e7
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-07-31 10:50:31 -0300

    Mais testes de formatação de cards com jQuery

--- js/card-response.js
+++ js/card-response.js
@@ -1,4 +1,17 @@
 
 $(document).ready(function(){
-    $("#_card").hide();
+    $("#card_body01").click(function(){
+        $(this).css('background-color', '#ffaf1f');
+        $("#card_01").removeClass('text-muted');
+    });
+
+    $("#card_body02").click(function(){
+        $(this).css('background-color', '#ffaf1f');
+        $("#card_02").removeClass('text-muted');
+    });
+
+    $("#card_body03").click(function(){
+        $(this).css('background-color', '#ffaf1f');
+        $("#card_03").removeClass('text-muted');
+    });
 })
