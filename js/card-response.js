7f8b2105b8bf130db02f70d96dabb5d5837c7e0e
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-07-31 18:27:28 -0300

    Alteração no jQuery do projeto

--- js/card-response.js
+++ js/card-response.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function(){
-    $("#card_body01").click(function(){
+    $("#card_01").click(function(){
         $(this).css('background-color', '#ffaf1f');
         $("#card_01").removeClass('text-muted');
     });
 
-    $("#card_body02").click(function(){
+    $("#card_02").click(function(){
         $(this).css('background-color', '#ffaf1f');
         $("#card_02").removeClass('text-muted');
     });
 
-    $("#card_body03").click(function(){
+    $("#card_03").click(function(){
         $(this).css('background-color', '#ffaf1f');
         $("#card_03").removeClass('text-muted');
     });
